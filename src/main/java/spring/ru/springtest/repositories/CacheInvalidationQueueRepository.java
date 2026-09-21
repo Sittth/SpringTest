067db9da471a3e80db08f16fc71e98f3d7f461ee
Eugene <evgeniy1mosolov@gmail.com>
2026-09-21 23:56:10 +0300

    Changed the @Param import in CacheInvalidationQueueRepository

--- src/main/java/spring/ru/springtest/repositories/CacheInvalidationQueueRepository.java
+++ src/main/java/spring/ru/springtest/repositories/CacheInvalidationQueueRepository.java
@@ -1,9 +1,9 @@
 package spring.ru.springtest.repositories;
 
-import feign.Param;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import spring.ru.springtest.models.CacheInvalidationQueueEntry;
 
 import java.time.OffsetDateTime;
